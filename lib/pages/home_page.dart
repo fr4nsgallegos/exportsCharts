886c7097efb00873966391a388985defbcb1f82f
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-03-02 12:29:38 -0500

    librariers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomePage extends StatelessWidget {
   CollectionReference candidateReference =
       FirebaseFirestore.instance.collection("partidosPolíticos");
+
+  exportPDF() {
+    final pdf = pw.Document();
+    pw.MultiPage(
+      pageFormat: PdfPageFormat.a4,
+      build: (pw.Context context) {
+        return [
+          pw.Text("Hola"),
+        ];
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     candidateReference.get().then((value) {
@@ -13,7 +28,18 @@ class HomePage extends StatelessWidget {
     });
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                exportPDF();
+              },
+              child: Text("TO PDF"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
