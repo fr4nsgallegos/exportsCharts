93342075dbe2c7e243938f2dd30ea212508e0f9a
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-03-02 12:04:08 -0500

    forebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:exportsandcharts/firebase_options.dart';
 import 'package:exportsandcharts/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       home: HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  CollectionReference candidateReference =
+      FirebaseFirestore.instance.collection("partidosPolíticos");
   @override
   Widget build(BuildContext context) {
+    candidateReference.get().then((value) {
+      QuerySnapshot candidateCollection = value;
+      List<QueryDocumentSnapshot> docs = candidateCollection.docs;
+      print(docs.length);
+    });
     return Scaffold(
       appBar: AppBar(),
       body: Column(),
