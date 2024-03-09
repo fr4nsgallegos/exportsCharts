1919cbed55895e4c81c1f9e672c890fe5bf3e51f
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-03-08 21:24:04 -0500

    gorouter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:exportsandcharts/pages/home_page.dart';
 import 'package:exportsandcharts/utils/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,10 +13,31 @@ void main() async {
   );
   await NotificationService.initMessaging();
 
-  runApp(
-    MaterialApp(
-      home: HomePage(),
+  runApp(MyApp()
+      // MaterialApp(
+      //   home: HomePage(),
+      //   debugShowCheckedModeBanner: false,
+      // ),
+      );
+}
+
+final GoRouter _goRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return HomePage();
+        }),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-    ),
-  );
+      routerConfig: _goRouter,
+    );
+  }
 }
