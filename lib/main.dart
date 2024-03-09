3603acace9bdf44599469fae87ab32ebcb8d0b30
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-03-08 19:59:52 -0500

    notifiactions init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:exportsandcharts/firebase_options.dart';
 import 'package:exportsandcharts/pages/home_page.dart';
+import 'package:exportsandcharts/utils/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await NotificationService.initMessaging();
+
   runApp(
     MaterialApp(
       home: HomePage(),
